7e1e03751cea8cd0a64cb8ae8a7eb86b19df62d1
Piotr Cianciara <piotr.cianciara@comatec.fi>
2022-06-01 13:31:22 +0200

    Dynamic movie list

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template
+from asyncio.proactor_events import _ProactorDuplexPipeTransport
+from flask import Flask, render_template, request
 import tmdb_client
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    movies = tmdb_client.get_movies(how_many=8)
-    return render_template("homepage.html", movies=movies)
-
+    selected_list = request.args.get('list_type', "popular")
+    movies = tmdb_client.get_movies(how_many=8, list_type = selected_list)
+    lista = ["now_playing", "popular", "top_rated", "upcoming"]
+    return render_template("homepage.html", movies=movies, current_list=selected_list, lista=lista)
 
 @app.context_processor
 def utility_processor():
@@ -14,5 +16,13 @@ def utility_processor():
         return tmdb_client.get_poster_url(path, size)
     return {"tmdb_image_url": tmdb_image_url}
 
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+    movie = tmdb_client.get_single_movie(movie_id)
+    cast = tmdb_client.get_single_movie_cast_20(how_many = 8, movie_id=movie_id)
+    return render_template("movie_details.html", movie=movie, cast=cast)
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True)  
+    app.run(debug=True) 
